ad71ba9f2babb599aa6685ac6b0544777d68b46f
Tizian Imseng <146072924+H14d3n@users.noreply.github.com>
2024-01-03 14:59:47 +0100

    Main Installer Script

--- ubuntu/apps.sh
+++ ubuntu/apps.sh
@@ -21,8 +21,8 @@ sudo add-apt-repository --remove ppa:yann1ck
 sudo rm /etc/systemd/user/default.target.wants/onedrive.service
 sudo rm -rf /var/lib/dpkg/lock-frontend
 sudo rm -rf /var/lib/dpkg/lock
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
 wget -qO - https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /usr/share/keyrings/obs-onedrive.gpg > /dev/null
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/obs-onedrive.gpg] https://download.opensuse.org/repositories/home:/npreining:/debian-ubuntu-onedrive/xUbuntu_22.04/ ./" | sudo tee /etc/apt/sources.list.d/onedrive.list
 sudo apt install -y python3
