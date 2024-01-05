89bebae55a7b951bcadacb7ca34647b944422f1a
Tizian Imseng <146072924+H14d3n@users.noreply.github.com>
2024-01-05 09:30:56 +0100

    Remove Main Apps

--- ubuntu/remove-apps.sh
+++ ubuntu/remove-apps.sh
@@ -1,2 +1,64 @@
 #!bin/bash
 
+# System auf den neuesten Stand bringen
+sudo apt update -y && sudo apt upgrade -y
+
+# Remove Software Properties
+sudo apt remove -y software-properties-common
+
+# Remove OFfice365 WebDesktop
+sudo snap remove office365webdesktop
+
+# Remove Teams
+sudo snap remove teams-for-linux
+
+# Remove OneDrive GUI:
+sudo rm /etc/apt/sources.list.d/onedrive.list
+sudo rm /usr/share/keyrings/obs-onedrive.gpg
+sudo rm /etc/apt/sources.list.d/obs-onedrive.list
+sudo apt remove onedrive
+sudo rm OneDriveGUI-1.0.3_fix116-x86_64.AppImage
+sudo rm -r ~/.config/onedrive
+sudo rm -r ~/.config/onedrive-gui
+
+# Remove ownCloud
+sudo apt remove owncloud-client
+sudo rm /etc/apt/sources.list.d/owncloud.list
+sudo rm /etc/apt/trusted.gpg.d/owncloud.gpg
+sudo apt remove apt-transport-https
+
+# Remove Notepad++
+sudo apt remove -y wine
+sudo snap remove notepad-plus-plus
+
+# Remove Zoom
+sudo snap remove zoom-client
+
+# Remove Flameshot
+sudo apt remove flameshot
+
+# Remove Gnome-Tweaks
+sudo apt remove gnome-tweaks
+
+# Remove fSearch
+sudo snap remove fsearch
+
+# Remove Intune
+sudo apt remove intune-portal
+sudo rm /etc/apt/sources.list.d/microsoft-ubuntu-jammy-prod.list
+sudo rm /usr/share/keyrings/microsoft.gpg
+
+
+# Remove Printers
+sudo lpadmin -x br-pr-ug
+sudo lpadmin -x br-pr-001
+sudo lpadmin -x br-pr-004
+sudo lpadmin -x br-pr-005
+
+# Add Locks again
+sudo touch /var/lib/dpkg/lock-frontend
+sudo touch /var/lib/dpkg/lock
+
+echo "All dependent Apps are now remove"
+echo "Press [Enter] to proceed"
+read
