82159ae901decb4422e7df707d73b7f55a5e7abe
Tizian Imseng <146072924+H14d3n@users.noreply.github.com>
2024-01-09 14:31:27 +0100

    Remove Main Apps

--- ubuntu/remove-apps.sh
+++ ubuntu/remove-apps.sh
@@ -55,9 +55,13 @@ sudo rm /usr/share/keyrings/microsoft.gpg
 
 # Remove Printers
 sudo lpadmin -x br-pr-ug
+sudo lpadmin -x br-pr-eg
 sudo lpadmin -x br-pr-001
 sudo lpadmin -x br-pr-004
 sudo lpadmin -x br-pr-005
+sudo lpadmin -x si-pr-001
+sudo lpadmin -x fr-pr-001
+sudo lpadmin -x pf-pr-001
 
 # Remove DisplayLink driver
 sudo apt-get remove -y --purge displaylink-driver
