8fda8b33d03efdc8222111cf58f5bd52ef1cecc4
Tizian Imseng <146072924+H14d3n@users.noreply.github.com>
2024-01-09 15:46:10 +0100

    Main Installer Script

--- ubuntu/apps.sh
+++ ubuntu/apps.sh
@@ -75,7 +75,7 @@ sudo apt install -y gnome-control-center
 # Add Printers
 sudo lpadmin -p br-pr-ug -E -v socket://10.5.20.25 -L "Campus Brig Drucker UG"
 sudo lpadmin -p br-pr-eg -E -v socket://10.5.20.24 -L "Campus Brig Drucker EG"
-sudo lpadmin -p br-pr-001 -E -v socket://10.5.20.23 -L "Campus Brig Drucker 1.Stock"
+sudo lpadmin -p br-pr-001 -E -v socket://10.5.20.23 -L "Campus Brig Drucker 1.Stock" -P ~/Automation/ubuntu/printerref/Ricoh-IM_C3000-Postscript-Ricoh.ppd
 sudo lpadmin -p br-pr-004 -E -v socket://10.5.20.22 -L "Campus Brig Drucker 4.Stock"
 sudo lpadmin -p br-pr-005 -E -v socket://10.5.20.21 -L "Campus Brig Drucker 5.Stock"
 sudo lpadmin -p si-pr-001 -E -v socket://10.2.30.36 -L "Standort Siders Drucker"
